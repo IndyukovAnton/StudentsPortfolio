0274ebdb3b4fb9b3457e2a261392bb3994cd71b9
Anton Indyukov <indyukovanton@gmail.com>
2024-05-25 14:13:32 +0300

    update: formAddWork

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 import {Lessons} from '../data/lessons.js';
 import {search} from './search.js';
 
-const postsContainer = document.querySelector('.main__posts');
+const postsContainer = document.querySelector('.main__content');
 const mainTitle = document.querySelector('.main__title');
 
 function createPost(author, comment, photoURL, link) {
@@ -32,6 +32,34 @@ function loadPosts(posts) {
 	});
 }
 
+// function loadFormAddWork() {
+// 	postsContainer.innerHTML = '';
+// 	return `
+// 	<form action="#" class="add-work">
+// 		<input type="text" placeholder='Автор' class='input__author input_field'>
+// 		<input type="text" placeholder='Комментарий' class='input__comment input_field'>
+// 		<div class="input__photo-wrapper input__wrapper">
+// 			<span>Добавьте фото</span>
+// 			<input type="file" style="display: none" id="getPhoto" class='input__photo'>
+// 			<button onclick="document.getElementById('getPhoto').click()">Выберите обложку</button>
+// 		</div>
+// 		<div class="input__file-wrapper input__wrapper">
+// 			<span>Добавьте проект</span>
+// 			<input type="file" style="display: none" id="getFile" class='input__file'>
+// 			<button onclick="document.getElementById('getFile').click()">Выберите файл</button>
+// 		</div>
+		
+// 	</form>
+// 	`
+// }
+
+function loadFormAddWorkDummy() {
+	postsContainer.innerHTML = '';
+	return `
+		<h2 style="text-align: center; font-size: 40px">Скоро...</h2>
+	`
+}
+
 lessonsContainer.addEventListener('click', (e) => {
 	if (e.target.classList.contains('lessons__item__link')) {
 		e.preventDefault();
@@ -41,8 +69,14 @@ lessonsContainer.addEventListener('click', (e) => {
 		lessonsContainer.classList.remove('open');
 		lessonsContainer.classList.add('close');
 
-		let postData = Lessons[e.target.href.split('/')[3]];
-		loadPosts(postData)
+		const path = e.target.href.split('/')
+		let page = path[path.length - 1]
+
+		if (page == 'add-work') {
+			postsContainer.insertAdjacentHTML('beforeend', loadFormAddWorkDummy());
+		} else {
+			loadPosts(Lessons[page])
+		}
 	}
 });
 
